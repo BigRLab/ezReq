d655243e7412e1e6086bd36ae6301180817411a8
urain39 <urain39@cyfan.cf>
2019-09-05 14:48:48 +0800

    update: use yapf format setup.py, test.py

--- setup.py
+++ setup.py
@@ -8,40 +8,38 @@ __email__ = "urain39@qq.com"
 __version__ = "0.0.8"
 __license__ = "MIT"
 
-
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 # Description
 with open('README') as fp:
-    long_description = fp.read()
+  long_description = fp.read()
 
 # Requirements
-with  open('requirements.txt') as fp:
-    requirements = fp.read().splitlines()
+with open('requirements.txt') as fp:
+  requirements = fp.read().splitlines()
 
-setup(name=__name__,
-      version=__version__,
-      description='Easy HttpClient Component for Python',
-      long_description=long_description,
-      author=__author__,
-      author_email=__email__,
-      license=__license__,
-      keywords=['request', 'requests', "http", "httpclient"],
-      url='https://github.com/urain39/EzReq',
-      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-      install_requires=[requirements],
-      platforms='any',
-      classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python",
+setup(
+    name=__name__,
+    version=__version__,
+    description='Easy HttpClient Component for Python',
+    long_description=long_description,
+    author=__author__,
+    author_email=__email__,
+    license=__license__,
+    keywords=['request', 'requests', "http", "httpclient"],
+    url='https://github.com/urain39/EzReq',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[requirements],
+    platforms='any',
+    classifiers=[
+        "Development Status :: 4 - Beta", "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Internet"
-        ],
-     )
+        "License :: OSI Approved :: MIT License", "Topic :: Internet"
+    ],
+)
